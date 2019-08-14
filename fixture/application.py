2509571acb60f1145a106540ca28c244079d44f3
asemerenko <anna.semerenko@djangostars.com>
2019-08-14 13:16:09 +0300

    Запуск разных браузеров

--- fixture/application.py
+++ fixture/application.py
@@ -4,14 +4,22 @@ from fixture.session import SessionHelper
 
 class Application:
     def __init__(self, browser, config):
-        if browser == "firefox":
-            self.wd = webdriver.Firefox()
+        if browser == "firefox-new":
+            self.wd = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
+        elif browser == "firefox-esr":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False},
+                                        firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox ESR\\firefox.exe")
+        elif browser == "firefox-nightly":
+            self.wd = webdriver.Firefox(firefox_binary="c:\\Program Files\\Firefox Nightly\\firefox.exe")
         elif browser == "chrome":
-            self.wd = webdriver.Chrome()
+            options = webdriver.ChromeOptions()
+            options.add_argument("start-maximized")
+            self.wd = webdriver.Chrome(chrome_options=options)
         elif browser == "ie":
-            self.wd = webdriver.Ie()
+            self.wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
         else:
             raise ValueError("Unrecognized browser %s" % browser)
+        print(self.wd.capabilities)
         self.session = SessionHelper(self)
         self.wd.implicitly_wait(3)
         self.config = config
